e2dc8087b5afb8b65553d62e3cf3d0e52822414f
Anthony DELILLE <a.delille982@gmail.com>
2023-12-28 00:31:42 +0100

    front part finished

--- assets/app.js
+++ assets/app.js
@@ -13,38 +13,55 @@ document.addEventListener('DOMContentLoaded', function () {
     var switchLabel = document.getElementById('toggleLabel');
     var menu1 = document.getElementById('menu1');
     var menu2 = document.getElementById('menu2');
+
+    var switchButton2 = document.getElementById('toggleSwitch2');
+    var switchLabel2 = document.getElementById('toggleLabel2');
+    var menu12 = document.getElementById('menu3');
+    var menu22 = document.getElementById('menu4');
+
     var navLinkHorizontal2 = document.querySelector('.nav-link-horizontal2');
     var acheterCredits = document.getElementById('acheterCredits');
 
-    // Récupérer l'état stocké ou utiliser la valeur par défaut
+    // Fonction pour mettre à jour l'état des menus
+    function updateMenus(isPartner) {
+        switchButton.checked = isPartner;
+        switchLabel.textContent = isPartner ? 'Partenaire' : 'Client';
+        menu1.style.display = isPartner ? 'none' : 'block';
+        menu2.style.display = isPartner ? 'block' : 'none';
+
+        switchButton2.checked = isPartner;
+        switchLabel2.textContent = isPartner ? 'Part.' : 'Client';
+        menu12.style.display = isPartner ? 'block' : 'none';
+        menu22.style.display = isPartner ? 'none' : 'block';
+
+        navLinkHorizontal2.style.display = isPartner ? 'block' : 'none';
+        acheterCredits.style.display = isPartner ? 'none' : 'block';
+    }
+
+    // Récupérer l'état stocké et initialiser les menus
     var isPartner = localStorage.getItem('isPartner') === 'true';
+    updateMenus(isPartner);
 
-    // Initialiser les éléments sur le chargement de la page
-    switchButton.checked = isPartner;
-    switchLabel.textContent = isPartner ? 'Partenaire' : 'Client';
-    menu1.style.display = isPartner ? 'none' : 'block';
-    menu2.style.display = isPartner ? 'block' : 'none';
-    navLinkHorizontal2.style.display = isPartner ? 'block' : 'none';
-    acheterCredits.style.display = isPartner ? 'none' : 'block';
-
-    // Événement de changement pour le switch
+    // Écouteurs d'événements pour les switches
     switchButton.addEventListener('change', function () {
         var isChecked = this.checked;
-        localStorage.setItem('isPartner', isChecked); // Stocker l'état
-        switchLabel.textContent = isChecked ? 'Partenaire' : 'Client';
-        menu1.style.display = isChecked ? 'none' : 'block';
-        menu2.style.display = isChecked ? 'block' : 'none';
-        navLinkHorizontal2.style.display = isChecked ? 'block' : 'none';
-        acheterCredits.style.display = isChecked ? 'none' : 'block';
+        localStorage.setItem('isPartner', isChecked);
+        updateMenus(isChecked);
     });
 
-    // Gestion du menu déroulant de recherche
+    switchButton2.addEventListener('change', function () {
+        var isChecked = this.checked;
+        localStorage.setItem('isPartner', isChecked);
+        updateMenus(isChecked);
+    });
+
+    // Gestion du menu déroulant de recherche pour le menu normal
     var navItem = document.querySelector('.nav-item');
     var navLink = navItem.querySelector('.nav-link.d-flex');
     var collapseMenu = navItem.querySelector('.collapse');
     var collapseIcon = navLink.querySelector('.collapse-icon');
 
-    collapseMenu.style.maxHeight = '0px'; // S'assurer que le menu est fermé au chargement
+    collapseMenu.style.maxHeight = '0px';
 
     navLink.addEventListener('click', function (event) {
         event.preventDefault();
@@ -53,6 +70,7 @@ document.addEventListener('DOMContentLoaded', function () {
         collapseIcon.classList.toggle('collapse-icon-open', !isCollapsed);
     });
 });
+
 
 
 document.addEventListener("DOMContentLoaded", function() {
